67f41d5ab4fca0b2724a65ca2d4eb508896ff859
ElenaKorkach <elena.birsan25@gmail.com>
2017-12-27 20:42:27 +0200

    gfmfm

--- server.js
+++ server.js
@@ -217,4 +217,7 @@ app.listen(3000, () => {
 
 
 
-//https://git-scm.com/book/ru/v1/%D0%92%D0%B2%D0%B5%D0%B4%D0%B5%D0%BD%D0%B8%D0%B5-%D0%A3%D1%81%D1%82%D0%B0%D0%BD%D0%BE%D0%B2%D0%BA%D0%B0-Git
+// //https://git-scm.com/book/ru/v1/%D0%92%D0%B2%D0%B5%D0%B4%D0%B5%D0%BD%D0%B8%D0%B5-%D0%A3%D1%81%D1%82%D0%B0%D0%BD%D0%BE%D0%B2%D0%BA%D0%B0-Git
+// git commit -m "first commit"
+// git remote add origin https://github.com/zheka-morskoi/test2.git
+// git push -u origin master
